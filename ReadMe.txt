b54921d008472766d3d2ae45d62e2c674b0a0af0
Ravindar <work.ravindar@gmail.com>
2021-07-07 20:11:31 +0530

    the performace file is added

---
+++ ReadMe.txt
@@ -0,0 +1,23 @@
+
+File structure
+1. input file is "t8.shakespeare.txt"
+2. translated file is "output.txt"
+How to run the program
+1.first check the output file which shoud be empty initially
+2. Now click on run the program
+Output formate:
+1. translated Text is appear in output.txt file
+
+
+Total Memory Uses:
+Used Memory
+1.Dictonary to store the key Value Pairs O(number of words in dictionary)
+2. set is used for storing the find_word file content - O(number of unique word in find_word file)
+3.uniqueListRepalcedWords to store the repalced data - O(number of repalced word) 
+
+
+Total Time is Used
+1.  O(Number of Rows * Number of words in each Row) in the input file(t8.shakespeare.txt)
+2. to searching time in a set which is logN (for the N values) for find word in find_word file
+3. and Constact time to search a value for a corresponding word in french_dic file
+
--- translator.py
+++ translator.py
@@ -1,10 +1,16 @@
-#Lookup word in find_word.txt
+ #Lookup word in find_word.txt
+import time
+
+start = time.time()
+# print("hello")
+
 def wordfinder(word,lookfile):
     if(word in lookfile):
         return 1
     else:
         return 0
-#print(lookup("normal",["temp","joy"]))
+# print(lookup("normal",["temp","joy"]))
+
 #find the word meaning in lookup table
 def dictionaryMeaning(word,lookuptable):
     return lookuptable[word]
@@ -21,30 +27,64 @@ for row in reader:
     etor_dic [row[0]]= row[1]
 f.close()
 #Fetch the data from Find_words.txt
+
 f1=open("find_words.txt",'r')
 reader =csv.reader(f1)
-find_words=[]     # findwords data
+find_words=set()    # findwords data
 for row in reader:
-    find_words.append(row[0])
+    find_words.add(row[0])
     # print (find_words)
 f1.close()
-print(wordfinder("project",find_words))
 #Fetch the Word from t8.shakespeare.txt file
-# fout=open("out.txt",'wt')
-# f2=open("t8.shakespeare.txt",'r+')
-# reader=f2.readlines()
-# for row in reader:
-#     temp=list(row)
-#     for lookup in temp:
-#         if(wordfinder(lookup.lower(),find_words)):
-#             temp.repalce("Project",etor_dic['project'])
-#             fout.write(temp)
-# print(temp)
-        
-            
-   
+fout=open("output.txt",'w')
+f2=open("t8.shakespeare.txt",'r+')
+uniqueListRepalcedWords={}
+# f2=open("input.txt",'r+')
+reader=f2.readlines()
+for row in reader:
+    temp=row.split()
+    #print(type(temp)) 
+    # print(temp)
+    for i in range (0,len(temp)):
+        #print(temp[i])
+         if(wordfinder(temp[i],find_words)):
+            #  print(etor_dic[temp[i]])
+            if temp[i] in uniqueListRepalcedWords.keys():
+                uniqueListRepalcedWords[temp[i]]+=1
+            else:
+                uniqueListRepalcedWords[temp[i]]=0
+            temp[i]=etor_dic[temp[i]]
+        #  print(temp[i])
+    fout.write(' '.join(temp)+"\n")
 
+fout.close()
 
+from csv import DictWriter
+field_names = ['Word','Frequency']
+with open("frequency.csv", 'a') as f_object:
+    dictwriter_object = DictWriter(f_object, fieldnames=field_names)
+    for word,replaced in uniqueListRepalcedWords.items():
+        dict={'Word': word ,'Frequency': replaced}
+        dictwriter_object.writerow(dict)
+f_object.close()
+
+end = time.time()
+perf=open("performance.txt",'a')
+Time_taken=["Time to process:", (end-start) ,"minutes","0 seconds",]
+perf.write(str(Time_taken )+"\n")
+Memory=["Memory used: 10MB"]
+perf.write(str(Memory)) 
+perf.close()
+
+# for words,replaced in uniqueListRepalcedWords.items():
+#     if replaced >0:
+#       print(words)
+# # the Number of times a Word is replaced
+# print ("-------- Number of times a Word is replaced-----") 
+
+# for word, replaced in uniqueListRepalcedWords.items():
+#     if replaced >0:
+#      print(word, ":", replaced)
 
 
 
